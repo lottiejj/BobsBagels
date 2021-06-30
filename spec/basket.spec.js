5c2ee6abcedc08da641efd4e6e2e6b622df3af55
Aamirah <aamz_333@hotmail.com>
2021-06-30 18:07:18 +0100

    update

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -12,15 +12,30 @@ result = basket.add("Banana Bagel")
 //verify 1 item
 console.log("Can add one item")
 console.log(assertEquals(result.length, 1))
-console,log(assertEquals(result.includes("Banana Bagel"), true))
+console.log(assertEquals(result.includes("Banana Bagel"), true))
+
+
 //setup 2 items
+basket = new Basket ()
 
 //execute 2 items
+result1 = basket.add("Banana Bagel")
+result2 = basket.add("Blueberry Bagel")
 
 //verify 2 items
+console.log("Can add two items")
+console.log(assertEquals(result2.length, 2))
+console.log(assertEquals(result1.includes("Banana Bagel"), true))
+console.log(assertEquals(result2.includes("Blueberry Bagel"), true))
 
 //setup remove item
+basket = new Basket ()
+basket.add("Banana Bagel")
 
 //execute remove item
+result = basket.remove("Banana Bagel")
 
 //verify remove item
+console.log("remove item")
+console.log(assertEquals(result.length, 0))
+console.log(assertEquals(result.includes("Banana Bagel"), false))
